7157db32acdf5c718e9107fcafd61b4c56518545
vegfjogs <vegard.fjogstad@finn.no>
2015-09-07 19:54:37 +0200

    Get started Google fit part done.

---
+++ app/src/main/java/com/fjoggs/tidfortrening/FitnessAPIFragment.java
@@ -0,0 +1,22 @@
+package com.fjoggs.tidfortrening;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FitnessAPIFragment extends Fragment {
+
+    public FitnessAPIFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fitness_api, container, false);
+    }
+}
