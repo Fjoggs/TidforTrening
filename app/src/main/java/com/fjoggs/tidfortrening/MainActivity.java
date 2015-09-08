360ac64c3ad429578a8e4ec06c32a68c664e94a4
vegfjogs <vegard.fjogstad@finn.no>
2015-09-08 23:23:58 +0200

    Removed Google Fit as Google is evil. Added a seperate module for API.

--- app/src/main/java/com/fjoggs/tidfortrening/MainActivity.java
+++ app/src/main/java/com/fjoggs/tidfortrening/MainActivity.java
@@ -1,57 +1,16 @@
 package com.fjoggs.tidfortrening;
 
-import android.content.Intent;
-import android.content.IntentSender;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.request.SessionReadRequest;
-
-import java.sql.Time;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int REQUEST_OAUTH = 1;
-
-    /**
-     * Track whether an authorization activity is stacking over the current activity, i.e. when
-     * a known auth error is being resolved, such as showing the account chooser or presenting a
-     * consent dialog. This avoids common duplications as might happen on screen rotations, etc.
-     */
-
-    private static final String AUTH_PENDING = "auth_state_pending";
-    private static final String TAG = "MainActivity";
-    private static final String SAMPLE_SESSION_NAME = "Sample Run";
-    private static final String DATE_FORMAT = "yyyy.MM.dd HH:mm:ss";
-
-    private boolean authInProgress = false;
-    private GoogleApiClient mClient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState != null) {
-            authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
-        }
-
-        buildFitnessClient();
-//        initiateHistory();
     }
 
     @Override
@@ -71,186 +30,7 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        } else if (id == R.id.action_readHistory) {
-            long startTimeTU = readFitnessSession().getStartTime(TimeUnit.MILLISECONDS);
-            String startTime = String.format("%d min, %d sec",
-                    TimeUnit.MILLISECONDS.toMinutes(startTimeTU),
-                    TimeUnit.MILLISECONDS.toSeconds(startTimeTU) -
-                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(startTimeTU))
-            );
-            long endTimeTU = readFitnessSession().getEndTime(TimeUnit.MILLISECONDS);
-            String endTime = String.format("%d min, %d sec",
-                    TimeUnit.MILLISECONDS.toMinutes(endTimeTU),
-                    TimeUnit.MILLISECONDS.toSeconds(endTimeTU) -
-                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(endTimeTU))
-            );
-
-            Toast.makeText(getApplicationContext(), readFitnessSession().getSessionName(), Toast.LENGTH_LONG).show();
-            Toast.makeText(getApplicationContext(), startTime
-                    , Toast.LENGTH_LONG).show();
-            Toast.makeText(getApplicationContext(), endTime
-                    , Toast.LENGTH_LONG).show();
         }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Build a {@link GoogleApiClient} that will authenticate the user and allow the application
-     * to connect to Fitness APIs. The scopes included should match the scopes your app needs
-     * (see documentation for details). Authentication will occasionally fail intentionally,
-     * and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
-     * can address. Examples of this include the user never having signed in before, or having
-     * multiple accounts on the device and needing to specify which account to use, etc.
-     */
-    private void buildFitnessClient() {
-        // Create the Google API Client
-        mClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.SENSORS_API)
-                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
-                .addConnectionCallbacks(
-                        new GoogleApiClient.ConnectionCallbacks() {
-
-                            @Override
-                            public void onConnected(Bundle bundle) {
-                                Log.i(TAG, "Connected!!!");
-                                // Now you can make calls to the Fitness APIs.
-                                // Put application specific code here.
-                            }
-
-                            @Override
-                            public void onConnectionSuspended(int i) {
-                                // If your connection to the sensor gets lost at some point,
-                                // you'll be able to determine the reason and react to it here.
-                                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
-                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
-                                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
-                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
-                                }
-                            }
-                        }
-                )
-                .addOnConnectionFailedListener(
-                        new GoogleApiClient.OnConnectionFailedListener() {
-                            // Called whenever the API client fails to connect.
-                            @Override
-                            public void onConnectionFailed(ConnectionResult result) {
-                                Log.i(TAG, "Connection failed. Cause: " + result.toString());
-                                if (!result.hasResolution()) {
-                                    // Show the localized error dialog
-                                    GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
-                                            MainActivity.this, 0).show();
-                                    return;
-                                }
-                                // The failure has a resolution. Resolve it.
-                                // Called typically when the app is not yet authorized, and an
-                                // authorization dialog is displayed to the user.
-                                if (!authInProgress) {
-                                    try {
-                                        Log.i(TAG, "Attempting to resolve failed connection");
-                                        authInProgress = true;
-                                        result.startResolutionForResult(MainActivity.this,
-                                                REQUEST_OAUTH);
-                                    } catch (IntentSender.SendIntentException e) {
-                                        Log.e(TAG,
-                                                "Exception while starting resolution activity", e);
-                                    }
-                                }
-                            }
-                        }
-                )
-                .build();
-    }
-
-//    private void initiateHistory() {
-//        // Setting a start and end date using a range of 1 week before this moment.
-//        Calendar cal = Calendar.getInstance();
-//        Date now = new Date();
-//        cal.setTime(now);
-//        long endTime = cal.getTimeInMillis();
-//        cal.add(Calendar.WEEK_OF_YEAR, -1);
-//        long startTime = cal.getTimeInMillis();
-//
-//        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-//        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
-//        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
-//
-//        DataReadRequest readRequest = new DataReadRequest.Builder()
-//                // The data request can specify multiple data types to return, effectively
-//                // combining multiple data queries into one call.
-//                // In this example, it's very unlikely that the request is for several hundred
-//                // datapoints each consisting of a few steps and a timestamp.  The more likely
-//                // scenario is wanting to see how many steps were walked per day, for 7 days.
-//                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-//                        // Analogous to a "Group By" in SQL, defines how data should be aggregated.
-//                        // bucketByTime allows for a time span, whereas bucketBySession would allow
-//                        // bucketing by "sessions", which would need to be defined in code.
-//                .bucketByTime(1, TimeUnit.DAYS)
-//                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-//                .build();
-//
-//        // Invoke the History API to fetch the data with the query and await the result of
-//        // the read request.
-//        DataReadResult dataReadResult =
-//                Fitness.HistoryApi.readData(mClient, readRequest).await(1, TimeUnit.MINUTES);
-//    }
-
-    /**
-     * Return a {@link SessionReadRequest} for all speed data in the past week.
-     */
-    private SessionReadRequest readFitnessSession() {
-        Log.i(TAG, "Reading History API results for session: " + SAMPLE_SESSION_NAME);
-        // [START build_read_session_request]
-        // Set a start and end time for our query, using a start time of 1 week before this moment.
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.WEEK_OF_YEAR, -1);
-        long startTime = cal.getTimeInMillis();
-
-        // Build a session read request
-        SessionReadRequest readRequest = new SessionReadRequest.Builder()
-                .setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS)
-                .read(DataType.TYPE_SPEED)
-                .setSessionName(SAMPLE_SESSION_NAME)
-                .build();
-        // [END build_read_session_request]
-
-        return readRequest;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Connect to the Fitness API
-        Log.i(TAG, "Connecting...");
-        mClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mClient.isConnected()) {
-            mClient.disconnect();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_OAUTH) {
-            authInProgress = false;
-            if (resultCode == RESULT_OK) {
-                // Make sure the app is not already connected or attempting to connect
-                if (!mClient.isConnecting() && !mClient.isConnected()) {
-                    mClient.connect();
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(AUTH_PENDING, authInProgress);
     }
 }
