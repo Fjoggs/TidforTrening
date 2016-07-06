1fd7be864fbd8abb65e98260bc7dca21f69250be
Vegard Fjogstad <fjoggis@gmail.com>
2016-07-06 18:49:01 +0200

    Removed modules not being used.

--- controller/src/main/java/com/fjoggs/tidfortrening/controller/Main.java
+++
@@ -1,9 +0,0 @@
-package com.fjoggs.tidfortrening.controller;
-
-import static spark.Spark.*;
-
-public class Main {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-}
