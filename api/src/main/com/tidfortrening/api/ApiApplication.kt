046ad3d56eead00cf7606c68f24f0540fcdf11f0
vegfjogs <vegard.fjogstad@finn.no>
2019-05-21 22:57:46 +0200

    Attempt to add Spring Boot

---
+++ api/src/main/com/tidfortrening/api/ApiApplication.kt
@@ -0,0 +1,8 @@
+package com.tidfortrening.api
+
+@SpringBootApplication
+class ApiApplication {
+    fun main(args: Array<String>) {
+        SpringApplication.run(ApiApplication::class.java, args)
+    }
+}
